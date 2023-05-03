b75121515adff3b21e85b4cc36db8361bfee377a
dustin1818 <dustinamoda18@gmail.com>
2023-05-03 23:23:53 +0800

    add episode page

--- episode/episode.js
+++ episode/episode.js
@@ -1,9 +1,25 @@
 "use strict";
+const episode_container = document.querySelector(".episode");
 
 function backPage() {
-    location.href = "../page/page.html";
+  location.href = "../page/page.html";
 }
 
-const animeEp = JSON.parse(localStorage.getItem("Episode-Details"));
+const getAnimeEpisode = async () => {
+  const animeEp = JSON.parse(localStorage.getItem("Episode-Details"));
+  const animeEpAttri = animeEp.attributes;
+  console.log(animeEpAttri);
+  const animePage = `
+  <img class = "w-50" src="${animeEpAttri.thumbnail.original}" alt="${animeEpAttri.thumbnail.original}">
+  <div class="anime-info"> 
+  <p><span class = "subheading">Title:</span>${animeEpAttri.canonicalTitle}</p>
+  <p><span class = "subheading">Description:</span>
+  <br><br>${animeEpAttri.description}</p>
+  <p><span class = "subheading">Rating: </span> ${animeEpAttri.airdate}</p>
+  <p><span class = "subheading">Season Number:</span>  ${animeEpAttri.seasonNumber}</p>
+  </div>
+  `;
+  episode_container.innerHTML = animePage;
+};
 
-console.log(animeEp);
+getAnimeEpisode();
